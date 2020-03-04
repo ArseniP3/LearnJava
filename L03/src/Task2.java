aa25348f002f40440e790d50a26de526a7fd753e
ArseniP3 <arseniplesko@gmail.com>
2020-03-04 07:19:37 +0300

    new tasks

--- L03/src/Task2.java
+++ L03/src/Task2.java
@@ -1,3 +1,7 @@
+/*
+2. Напишите программу, заносящую в массив первые 100 натуральных чисел,
+делящихся на 13 или на 17, и печатающую его.
+ */
 public class Task2 {
     public static void main(String[] args) {
         int[] array = new int[100];
--- L03/src/Task3.java
+++ L03/src/Task3.java
@@ -1,16 +1,46 @@
+/*
+3. Сгенерировать массив вещественных чисел из 1000 элементов
+(для генерации использовать Random random = new Random(); random.nextDouble()).
+Отсортировать массив, найти среднее значение, максимальное и минимальное.
+ */
 import java.util.Random;
 
-public class Task3 {
-
+public class Task3{
     public static void main(String[] args) {
         Random random = new Random();
-        int k = 1;
         double[] array = new double[1000];
-        for (double index = 0; index <=1000; index++){
-            array[index] = random.nextDouble();
-            System.out.println(k +  "  -  "+ array [index]);
-            k++;
-
+        int k = 0;
+        int index;
+        for (index = 0; index < array.length; index++ ){
+           array[(int) index] = random.nextDouble();
         }
+        for (index = 0; index < array.length; index++) {
+            for (int j = 0; j < array.length - index - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    double temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for (index = 0; index < array.length; index++) {
+            k++;
+            System.out.println(k + " : " + array[index]);
+        }
+        double max = array [0];
+        double min = array [0];
+        double sredn = 0;
+        for (index = 0; array.length > index; index++) {
+            if (max < array[index])
+                max = array[index];
+            if (min > array[index])
+                min = array[index];
+            sredn += array[index]/array.length;
+        }
+        System.out.println("Максимальное значение: " + max);
+        System.out.println("Минимальное значение: " + min);
+        System.out.println("Среднее значение: " + sredn);
+
     }
 }
