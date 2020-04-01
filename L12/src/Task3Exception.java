ce4fe861146866e967ca54e314f815b0bb14af8e
ArseniP3 <arseniplesko@gmail.com>
2020-04-01 17:42:23 +0300

    Finish

---
+++ L12/src/Task3Exception.java
@@ -0,0 +1,22 @@
+
+public class Task3Exception extends Exception{
+
+    public Task3Exception() {
+    }
+
+    public Task3Exception(String message) {
+        super(message);
+    }
+
+    public Task3Exception(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public Task3Exception(Throwable cause) {
+        super(cause);
+    }
+
+    public Task3Exception(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
