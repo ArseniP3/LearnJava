b5dbfde4194b9748bfd0eefa90da77f5848870b2
ArseniP3 <arseniplesko@gmail.com>
2020-04-07 17:06:48 +0300

    Finish

---
+++ L09/src/Task3_Solution.java
@@ -0,0 +1,9 @@
+/*
+3.	Дан класс Solution, параметризированный T. Ограничьте параметр T.
+T должен быть наследником класса ClassForGenerics и одновременно реализовывать интерфейс InterfaceForGenerics.
+Менять можно только класс Solution.
+
+ */
+public class Task3_Solution <T extends ClassForGenerics & InterfaceForGenerics> {
+
+}
